b6701fbd6f7f5062adf12c18b67035ff75fb37e5
kuo22 <kuohong22@gmail.com>
2019-05-06 21:19:05 -0700

    Updates

---
+++ precache-manifest.b230740ec2905581ef02f8a6c77be5e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2339cd0a85dc1aef76a541caa9051fa",
+    "url": "/pokemon-cries/index.html"
+  },
+  {
+    "revision": "2a7537997bae731e216a",
+    "url": "/pokemon-cries/static/css/main.1aa68c12.chunk.css"
+  },
+  {
+    "revision": "e1df9e5dd97f9dc5b41a",
+    "url": "/pokemon-cries/static/js/2.373ccd69.chunk.js"
+  },
+  {
+    "revision": "2a7537997bae731e216a",
+    "url": "/pokemon-cries/static/js/main.eb4a906b.chunk.js"
+  },
+  {
+    "revision": "26efea8e37ab66b445f1",
+    "url": "/pokemon-cries/static/js/runtime~main.0f0f56f1.js"
+  },
+  {
+    "revision": "4ca88767e1362c7a3c4afb72c8bc5849",
+    "url": "/pokemon-cries/static/media/pokemon_pixel_font.4ca88767.ttf"
+  },
+  {
+    "revision": "c74a590b9b702ce5970a3a34aa7d1823",
+    "url": "/pokemon-cries/static/media/speaker.c74a590b.svg"
+  }
+]);
